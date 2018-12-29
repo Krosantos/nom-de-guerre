d52c897a2bd65173ab1090567c57059bcbdfd59b
Tymko Tecca <tymko.tecca@gmail.com>
2018-12-29 00:26:24 -0500

    Modify SVGs

---
+++ src/client/styled/core.js
@@ -0,0 +1,68 @@
+import styled, { css } from 'styled-components';
+import {
+	active,
+	borderColor,
+	borderRadius,
+	borders,
+	boxShadow,
+	color,
+	disabled,
+	focus,
+	fontSize,
+	fontWeight,
+	hover,
+	space,
+	width,
+} from 'styled-system';
+import BaseComponent from './BaseComponent';
+import cssProps from './cssProps';
+
+const commonStyles = css`
+	${hover};
+	${active};
+	${borderRadius};
+	${borders};
+	${borderColor};
+	${color};
+	${disabled};
+	${focus};
+	${fontSize};
+	${boxShadow};
+	${space};
+	${fontWeight};
+	${width};
+	${Object.keys(cssProps).map((key) => cssProps[key])};
+`;
+
+export const Box = styled(BaseComponent('div'))`
+    ${commonStyles};
+`;
+Box.displayName = 'Box';
+
+export const Text = styled(BaseComponent('span'))`
+	${commonStyles};
+`;
+Text.displayName = 'Text';
+
+export const Button = styled(BaseComponent('button'))`
+	${commonStyles};
+`;
+Button.defaultProps = { outline: '0px' };
+Button.displayName = 'Button';
+
+export const Image = styled(BaseComponent('img'))`
+	${commonStyles};
+`;
+Image.defaultProps = { alt: '' };
+Image.displayName = 'Image';
+
+export const StyledSvg = styled(BaseComponent('svg'))`
+${commonStyles};
+`;
+StyledSvg.defaultProps = { alt: '' };
+StyledSvg.displayName = 'Svg';
+
+export const Path = styled(BaseComponent('path'))`
+${commonStyles};
+`;
+Path.displayName = 'Path';
--- src/client/styled/index.js
+++ src/client/styled/index.js
@@ -1,68 +1,16 @@
-import styled, { css } from 'styled-components';
+import Svg from './Svg';
 import {
-	active,
-	borderColor,
-	borderRadius,
-	borders,
-	boxShadow,
-	color,
-	disabled,
-	focus,
-	fontSize,
-	fontWeight,
-	hover,
-	space,
-	width,
-} from 'styled-system';
-import BaseComponent from './BaseComponent';
-import cssProps from './cssProps';
+	Box,
+	Text,
+	Button,
+	Image,
+} from './core';
 
-const commonStyles = css`
-	${hover};
-	${active};
-	${borderRadius};
-	${borders};
-	${borderColor};
-	${color};
-	${disabled};
-	${focus};
-	${fontSize};
-	${boxShadow};
-	${space};
-	${fontWeight};
-	${width};
-	${Object.keys(cssProps).map((key) => cssProps[key])};
-`;
+export {
+	Box,
+	Text,
+	Button,
+	Image,
+	Svg,
+};
 
-export const Box = styled(BaseComponent('div'))`
-    ${commonStyles};
-`;
-Box.displayName = 'Box';
-
-export const Text = styled(BaseComponent('span'))`
-	${commonStyles};
-`;
-Text.displayName = 'Text';
-
-export const Button = styled(BaseComponent('button'))`
-	${commonStyles};
-`;
-Button.defaultProps = { outline: '0px' };
-Button.displayName = 'Button';
-
-export const Image = styled(BaseComponent('img'))`
-	${commonStyles};
-`;
-Image.defaultProps = { alt: '' };
-Image.displayName = 'Image';
-
-export const Svg = styled(BaseComponent('svg'))`
-${commonStyles};
-`;
-Svg.defaultProps = { alt: '' };
-Svg.displayName = 'Svg';
-
-export const Path = styled(BaseComponent('path'))`
-${commonStyles};
-`;
-Path.displayName = 'Path';
