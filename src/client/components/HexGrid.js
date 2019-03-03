8e6b9d0bdd951be9259f23b9594abca79ef6a706
Krosantos <tymko.tecca@gmail.com>
2019-03-02 23:54:49 -0500

    Block-display HexGrid

--- src/client/components/HexGrid.js
+++ src/client/components/HexGrid.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Box, Hex } from '@core';
 import { times } from 'lodash';
 
+const calcBoxSize = (x, y, size) => {
+	const rawHeight = y * size * 0.9125;
+	const rawWidth = x * size * 0.8;
+	const height = `${rawHeight}px`;
+	const width = `${rawWidth}px`;
+
+	return { height, width };
+};
+
 const calcHexPosition = (x, y, size) => {
 	const xPosition = x * size * 0.73;
 
@@ -25,28 +34,32 @@ const calcHexZIndex = (x, maxX, y) => {
 	return zIndex < 0 ? 0 : zIndex;
 };
 
-const HexGrid = ({ x, y, size }) => (
-	<Box>
-		{times(x, (xIndex) => times(y, (yIndex) => {
-			const { xPosition, yPosition } = calcHexPosition(xIndex, yIndex, size);
-			const zIndex = calcHexZIndex(xIndex, x, yIndex, y);
-			const dimension = `${size}px`;
-			const key = `${xIndex}_${yIndex}`;
+const HexGrid = ({ x, y, size }) => {
+	const { height, width } = calcBoxSize(x, y, size);
 
-			return (
-				<Hex
-					key={key}
-					height={dimension}
-					left={xPosition}
-					top={yPosition}
-					width={dimension}
-					x={xIndex}
-					y={yIndex}
-					zIndex={zIndex}
-				/>
-			);
-		}))}
-	</Box>
-);
+	return (
+		<Box height={height} position="relative" width={width}>
+			{times(x, (xIndex) => times(y, (yIndex) => {
+				const { xPosition, yPosition } = calcHexPosition(xIndex, yIndex, size);
+				const zIndex = calcHexZIndex(xIndex, x, yIndex, y);
+				const dimension = `${size}px`;
+				const key = `${xIndex}_${yIndex}`;
+
+				return (
+					<Hex
+						key={key}
+						height={dimension}
+						left={xPosition}
+						top={yPosition}
+						width={dimension}
+						x={xIndex}
+						y={yIndex}
+						zIndex={zIndex}
+					/>
+				);
+			}))}
+		</Box>
+	);
+};
 
 export default HexGrid;
--- src/client/pages/EditArmies/components/Army/Army.js
+++ src/client/pages/EditArmies/components/Army/Army.js
@@ -3,7 +3,16 @@ import { Box } from '@core';
 import HexGrid from '@components/HexGrid';
 
 const Army = () => (
-	<Box bg="wine" flex="0 0 auto" height="100%" id="I AM THE COHORT AND AUXILIARY" width="375px">
+	<Box
+		alignItems="center"
+		bg="wine"
+		display="flex"
+		flex="0 0 auto"
+		height="100%"
+		id="I AM THE COHORT AND AUXILIARY"
+		justifyContent="center"
+		width="375px"
+	>
 
 		<HexGrid size={100} x={4} y={8} />
 	</Box>
